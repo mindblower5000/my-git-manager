a7c71287eef8bb3c4c559479c7271ab8fa3fc07b
Andrey Melchouk <ameltchouk@gmail.com>
2018-10-11 17:58:17 +0300

    Workin Fragment Templates

--- app/src/main/java/com/melchuk/mygitmanager/MainActivity.java
+++ app/src/main/java/com/melchuk/mygitmanager/MainActivity.java
@@ -1,8 +1,12 @@
 package com.melchuk.mygitmanager;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.melchuk.mygitmanager.dummy.DummyContent;
 
 
 //По поводу общего дз для проекта курса. На данный момент нужно создать проект с:
@@ -23,7 +29,7 @@ import android.view.MenuItem;
 //        6. На экране списка репошек добавить возможность поиска, слушать через rx
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, UserDataFragment.OnFragmentInteractionListener, ReposItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,22 +95,43 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Fragment fragment=null;
+        Class fragmentClass=null;
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_user) {
+            fragmentClass=UserDataFragment.class;
+        } else if (id == R.id.nav_repos) {
+            fragmentClass=ReposItemFragment.class;
         }
+
+        try{
+            fragment = (Fragment) (fragmentClass != null ? fragmentClass.newInstance() : null);
+        } catch (NullPointerException|IllegalAccessException|InstantiationException e){
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if(fragment==null) {
+            Log.d("MY_TAG","No fragment has been created.");
+        }else {
+            Log.d("MY_TAG","Fragment exists.");
+        }
+
+        fragmentManager.beginTransaction().replace(R.id.container,fragment).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
